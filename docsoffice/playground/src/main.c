ee9641ec6e227e03c69340926f32281d8c441c65
thedizzle69 <134519518+thedizzle69@users.noreply.github.com>
2023-06-03 21:07:59 +0200

    Add files via upload

--- docsoffice/playground/src/main.c
+++ docsoffice/playground/src/main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-#include "main.h"
+#include <string.h>
+#include "patient.h"
+#include "queue.h"
 #include "menu.h"
+#include "switchcase.h"
 
-void displayMenu();
+extern void loadPatients();
+extern void loadQueue();
+extern void savePatients();
+extern void saveQueue();
+extern void switchcase();
 
 int main() {
-    displayMenu();
+    // Load data
+    loadPatients();
+    loadQueue();
+
+    // Perform operations
+    switchcase();
+
+    // Save data
+    savePatients();
+    saveQueue();
+
     return 0;
 }
