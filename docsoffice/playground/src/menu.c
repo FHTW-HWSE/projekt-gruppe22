b97c6afda6026ba714268fab0bb4dca175aab385
thedizzle69 <134519518+thedizzle69@users.noreply.github.com>
2023-06-03 21:12:36 +0200

    Update menu.c

--- docsoffice/playground/src/menu.c
+++ docsoffice/playground/src/menu.c
@@ -5,18 +5,21 @@
 #include <sys/wait.h>
 #include <ctype.h>
 #include "menu.h"
+#include "switchcase.h"
+#include "patient.h"
+#include "queue.h"
+#include "searchPatient.h"
 
 void displayMenu() {
     printf("Welcome to the Doctor's Office Management System!\n");
     printf("Please select an option:\n");
-    printf("1. Search for a patient\n");
-    printf("2. Edit patient information\n");
-    printf("3. Add a patient\n");
-    printf("4. Add a patient to the queue\n");
-    printf("5. View patient queue\n");
-    printf("6. Assign a seat to a patient\n");
-    printf("7. Display seating arrangement\n");
-    printf("8. Exit\n");
-    printf("Enter your choice");
+    printf("1.	Search for a patient\n");
+    printf("2.  	Add a patient\n");  		//Dafuq
+    printf("3.	Edit patient information\n");
+    printf("4.	Add a patient to the queue\n");
+    printf("5.	View patient queue\n");
+    printf("6.	Assign a seat to a patient\n");
+    printf("7.	Display seating arrangement\n");
+    printf("0.	Exit\n");
     fflush(stdout);  // Ensure the output is flushed immediately
 }
