f759cb7f120e09391d48694f979313da3dea01d5
thedizzle69 <134519518+thedizzle69@users.noreply.github.com>
2023-06-13 17:12:30 +0200

    Final version for now

---
+++ docsoffice/src_final/patient.h
@@ -0,0 +1,30 @@
+#ifndef PATIENT_H
+#define PATIENT_H
+
+#define MAX_PATIENTS 100
+
+typedef struct {
+    char insuranceNumber[20];
+    char firstName[50];
+    char lastName[50];
+    char birthdate[20];
+    char street[50];
+	char streetno[20];
+    char zipCode[10];
+    char city[50];
+    float height;
+    float weight;
+} Patient;
+
+extern Patient* patients;
+extern int numPatients;
+extern int maxPatients;
+
+void loadPatients();
+void savePatients();
+void cleanup();
+void addPatient();
+
+
+#endif /* PATIENT_H */
+
